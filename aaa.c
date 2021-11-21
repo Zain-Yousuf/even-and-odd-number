a357d440b50bdd2e08a617a5a0888d19a80ea10e
Zain-Yousuf <93894509+Zain-Yousuf@users.noreply.github.com>
2021-11-21 11:16:21 +0500

    Add files via upload

---
+++ aaa.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+ int number;
+ printf("enter the enteger:");
+ scanf("%d",&number);
+ if(number%2==0){
+ printf("%d this is an even integer :",number);}
+ else{
+ printf("%d this is an odd integer:",number);}
+ 
+}
+
